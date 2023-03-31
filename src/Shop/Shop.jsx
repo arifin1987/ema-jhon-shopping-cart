5fddd607795e901a58ceff7ddc398e5fcd5d5924
Shohidul Arifin <shohidularifin@gmail.com>
2023-03-31 00:07:27 -0700

    first commit

---
+++ src/Shop/Shop.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import Cart from '../components/Cart/Cart';
+import Product from '../Product/Product';
+import { addToDb, getShoppingCart } from '../utilities/fakedb';
+import './Shop.css'
+
+const Shop = () => {
+    const[products,setProducts]= useState([])
+    const[cart,setCart]= useState([])
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=> res.json())
+        .then(data=> setProducts(data))
+
+    },[])
+
+    useEffect(()=>{
+        let storedCart = getShoppingCart();
+        let savedCart =[]
+        // step 1: get id
+        for(const id in storedCart){
+            // step:2 get the product by using id
+            const addedProduct = products.find(product =>product.id ==id)
+            // step 3: get quantity of the product
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }
+        setCart(savedCart)
+    },[products])
+
+    const handleAddToCart =(product)=>{
+        const newCart =[...cart,product];
+        setCart(newCart);  
+        addToDb(product.id);
+
+    }
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product=> <Product
+                    key={product.id}
+                    product ={product}
+                    handleAddToCart= {handleAddToCart}
+                    ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+               <Cart cart={cart}></Cart>
+                
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Shop;
